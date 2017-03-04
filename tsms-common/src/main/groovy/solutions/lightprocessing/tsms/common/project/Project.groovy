a41bf89f12142259dcde989d74eda24f94a52f9b
Anton Dzodzikov <anton.dzodzikov@gmail.com>
2017-03-04 20:46:00 +1000

    Add basic application structure

--- tsms-common/src/main/groovy/solutions/lightprocessing/tsms/common/project/Project.groovy
+++ tsms-common/src/main/groovy/solutions/lightprocessing/tsms/common/project/Project.groovy
@@ -1,6 +1,13 @@
 package solutions.lightprocessing.tsms.common.project
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
 class Project {
+    @Id
+    @GeneratedValue
     Long id
     String name
     String description
--- tsms-common/src/main/groovy/solutions/lightprocessing/tsms/common/project/ProjectVersion.groovy
+++ tsms-common/src/main/groovy/solutions/lightprocessing/tsms/common/project/ProjectVersion.groovy
@@ -1,7 +1,18 @@
 package solutions.lightprocessing.tsms.common.project
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+@Entity
 class ProjectVersion {
+    @Id
+    @GeneratedValue
     Long id
+
     String name
-    Long projectId
+
+    @ManyToOne
+    Project project
 }
