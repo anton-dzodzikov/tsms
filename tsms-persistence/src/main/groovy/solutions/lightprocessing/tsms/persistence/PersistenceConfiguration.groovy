3a4e4aac713495dbaa8f8bfac52b2139c25c8abf
Anton Dzodzikov <anton.dzodzikov@gmail.com>
2017-03-05 17:04:29 +1000

    Add persistence config

---
+++ tsms-persistence/src/main/groovy/solutions/lightprocessing/tsms/persistence/PersistenceConfiguration.groovy
@@ -0,0 +1,10 @@
+package solutions.lightprocessing.tsms.persistence
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaRepositories
+@EntityScan("solutions.lightprocessing.tsms.common")
+class PersistenceConfiguration {}
